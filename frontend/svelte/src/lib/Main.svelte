55e64d567a97cef6c574e88435f795cc3a255bcd
apatika <n123494@yandex.ru>
2023-11-26 14:05:22 +0300

    main svelte component

---
+++ frontend/svelte/src/lib/Main.svelte
@@ -0,0 +1,35 @@
+<script>
+  
+</script>
+
+<div id="container">
+  <div id="content">
+    <div id="table">
+
+    </div>
+  </div>
+  <div id="info">
+
+  </div>
+</div>
+
+<style>
+  #container{
+    display: flex;
+    height: 100%;
+  }
+  #content{
+    flex-grow: 1;
+    border-right: 1px solid black;
+    margin: 10px 5px;
+    padding: 10px 5px;
+  }
+  #table{
+    margin: 10px 5px;
+    border: 1px solid black;
+    height: 100%;
+  }
+  #info{
+    flex-basis: 30%;
+  }
+</style>
